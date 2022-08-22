abf25632269b97add3c022063389bf360caca939
Renato Mello <renatomdemello@gmail.com>
2022-08-22 11:00:56 -0300

    Adding entity and resource.

---
+++ src/main/java/com/renatomellotech/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.renatomellotech.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.renatomellotech.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Renato", "renatomdemello@gmail.com", "123456789", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
